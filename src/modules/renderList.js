01332bd04687b02061bb1a2cf00ad4c406740ebe
mutai-gilbert <mutaigilbert85@gmail.com>
2023-02-25 21:46:56 +0300

    Made changes to the index.js file

--- src/modules/renderList.js
+++ src/modules/renderList.js
@@ -11,9 +11,9 @@ const renderAddedList = (addedList) => {
   const completed = list.completed === true ? '-done' : '';
   listContainer.innerHTML += `
     <div class="list-group">
-        <div class="list-cont d-flex justify-content-center align-items-center m-2">
-            <input type="checkbox" name="checkbox" class="checkboxlabel border-0 text-lg" id="checkbox-${list.index}" ${isCompleted}>
-            <input type="text" readonly="true" class="checkbox-task ${isCompleted} border-0 text-lg" id="input${list.index}" value="${list.description}">
+        <div class="list-cont ">
+            <input type="checkbox" name="checkbox" class="checkboxlabel" id="checkbox-${list.index}" ${isCompleted}>
+            <input type="text" readonly="true" class="checkbox-task ${isCompleted}" id="input${list.index}" value="${list.description}">
             <div class="menu${completed}"><img src="${threeDot}" class="menu-img" id="menu-${list.index}"> <img src="${trash}" class="bin" id="${list.index}"></div>
             </div>
         </div>
